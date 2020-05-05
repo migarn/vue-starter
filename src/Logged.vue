2aea3708f211a96dab2e4f46623a0c9a288ca375
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-05 20:37:14 +0200

    Buttons in Logged component defined

--- src/Logged.vue
+++ src/Logged.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-		<h2>Witaj {{ username }}!<button @click="$emit('logout')" style="float:right;">Wyloguj</button></h2>
+		<h2>Witaj {{ username }}!<button @click="$emit('logout')"
+			style="float:right;" class="button button-outline">Wyloguj</button></h2>
+		<button>Dodaj nowe spotkanie</button>
         
     </div>
 </template>
