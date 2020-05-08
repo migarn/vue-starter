4aa6c94502affb5d6f8aba2c14f8adcdcd843d9d
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-08 20:45:03 +0200

    Wypisz się working correctly

--- src/meetings/MeetingHandler.vue
+++ src/meetings/MeetingHandler.vue
@@ -1,10 +1,10 @@
 <template>
-	<div v-if="this.meeting.participants.length > 0" style="float:right;">
-		<button class="button button-outline">Wypisz się</button>
-	</div>
-	<div v-else style="float:right;">
+	<div v-if="this.meeting.participants.indexOf(this.username) === -1" style="float:right;">
 		<button class="button button-outline" @click="signUp()">Zapisz się</button>
 		<button style="margin-left:5px;">Usuń puste spotkanie</button>
+	</div>
+	<div v-else style="float:right;">
+		<button class="button button-outline">Wypisz się</button>
 	</div>
 </template>
 
@@ -19,6 +19,7 @@
             signUp() {
 				if (this.meeting.participants.indexOf(this.username) === -1) {
 					this.meeting.participants.push(this.username);
+					this.$forceUpdate();
 				}
 				this.$emit('signUp');
             }
